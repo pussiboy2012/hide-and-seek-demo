d66286f62f8e1ac6264b0e2ddce345cb793610ee
pussiboy2012 <normarianin33@gmail.com>
2024-11-05 00:16:01 +0300

    refresh

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
+// src/firebase.js
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Конфигурация Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBMA3nCfa7v3nim319E3RIVMnjyzjN-6B4",
   authDomain: "has-mk.firebaseapp.com",
@@ -16,6 +13,9 @@ const firebaseConfig = {
   measurementId: "G-119PGT715D"
 };
 
-// Initialize Firebase
+// Инициализация Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Инициализация Firestore
+const db = getFirestore(app);
+export { db };
